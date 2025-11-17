de24878c22a8722d3aa592503f1e1e1d21e24db0
dzoniD <edvard_dantes@hotmail.com>
2025-11-17 01:00:09 +0100

    created 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Error404 } from './shared/components/error404/error404';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'home', component: Home },
+  {
+    path: 'not-found',
+    component: Error404,
+  },
+  { path: '**', redirectTo: '/not-found' },
 ];
